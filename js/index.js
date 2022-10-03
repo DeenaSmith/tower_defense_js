87f62eb869bf084446bd21afff7a8b604689550d
Deena Smith <deenasmith1630@gmail.com>
2022-10-03 15:19:52 -0500

    collision detection for building placement functional, refactored classes

--- js/index.js
+++ js/index.js
@@ -14,15 +14,7 @@ for (let i = 0; i < placementTilesData.length; i += 20) {
     placementTilesData2D.push(placementTilesData.slice(i, i + 20))
 }
 
-class PlacementTile {
-    constructor({position = {x: 0, y: 0}}) {
-        this.position = position
-        this.size = 64
-    }
-    draw() {
-        c.fillRect(this.position.x, this.position.y, this.size, this.size)
-    }
-}
+
 
 const placementTiles = [];
 
@@ -51,48 +43,6 @@ image.onload = () => {
 
 image.src = 'img/gameMap.png'
 
-class Enemy {
-    constructor({position = { x: 0, y: 0 }}) {
-        this.position = position;
-        this.width = 100;
-        this.height = 100;
-        this.waypointIndex = 0;
-        this.center = {
-            x: this.position.x + this.width / 2,
-            y: this.position.y + this.height / 2
-        }
-    }
-// how the enemy looks
-    draw() {
-        c.fillStyle = 'red';
-        c.fillRect(this.position.x, this.position.y, this.width, this.height);
-    }
-// enemy movement along waypoints
-    update() {
-        this.draw()
-
-        const waypoint = waypoints[this.waypointIndex]
-        const yDistance = waypoint.y - this.center.y;
-        const xDistance = waypoint.x - this.center.x;
-        const angle = Math.atan2(yDistance, xDistance);
-
-        this.position.x += Math.cos(angle);
-        this.position.y += Math.sin(angle);
-        this.center = {
-            x: this.position.x + this.width / 2,
-            y: this.position.y + this.height / 2
-        }
-
-        if (
-            Math.round(this.center.x) === Math.round(waypoint.x) && 
-            Math.round(this.center.y) === Math.round(waypoint.y) &&
-            this.waypointIndex < waypoints.length -1
-            ) {
-            this.waypointIndex++
-        }
-    }
-}
-
 const enemies = [];
 
 for (let i = 1; i < 10; i++) {
@@ -112,6 +62,20 @@ function animate() {
     enemies.forEach(enemy => {
         enemy.update()
     })
+
+    placementTiles.forEach((tile) => {
+        tile.update(mouse)
+    })
 };
+
+const mouse = {
+    x: undefined,
+    y: undefined
+}
+
+window.addEventListener('mousemove', (event) => {
+    mouse.x = event.clientX
+    mouse.y = event.clientY
+});
 
 animate();
