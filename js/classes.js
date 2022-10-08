fbcd7fd943498586671a542841dd0bd6d515176b
Deena Smith <deenasmith1630@gmail.com>
2022-10-08 10:15:35 -0500

    projectile spawns at center of building

--- js/classes.js
+++ js/classes.js
@@ -87,11 +87,16 @@ class Building {
     constructor({ position = { x: 0, y: 0 } }) {
         this.position = position
         this.width = 64 * 2;
+        this.height = 64
+        this.center = {
+            x: this.position.x + this.width / 2,
+            y: this.position.y + this.height / 2
+        }
         this.projectiles = [
             new Projectile({
                 position: {
-                    x: this.position.x,
-                    y: this.position.y
+                    x: this.center.x,
+                    y: this.center.y
                 }
             })
         ]
